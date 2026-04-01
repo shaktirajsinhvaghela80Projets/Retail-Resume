4774f7626634d182a0c73a9de66a3f91e2ac0e45
shaktirajsinhvaghela80Projets <shaktirajsinhvaghela80@gmail.com>
2026-03-31 18:36:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function downloadResume() {
+function downloadPDF() {
     window.print();
+}
+
+function downloadWord() {
+    let content = document.getElementById("resume").innerHTML;
+
+    let html = `
+    <html xmlns:o='urn:schemas-microsoft-com:office:office' 
+    xmlns:w='urn:schemas-microsoft-com:office:word'>
+    <head><meta charset='utf-8'></head>
+    <body>${content}</body></html>`;
+
+    let blob = new Blob(['\ufeff', html], {
+        type: 'application/msword'
+    });
+
+    let url = URL.createObjectURL(blob);
+
+    let link = document.createElement("a");
+    link.href = url;
+    link.download = "Shaktirajsinh_Vaghela_Resume.doc";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
